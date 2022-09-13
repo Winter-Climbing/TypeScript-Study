083fd8afa29858c19f04ab028f13e5639e2dc22c
Gose <niabc70@gmail.com>
2022-09-13 20:24:59 +0900

    function 주석 추가

--- TS-Basic/1-type/1-2-function.ts
+++ TS-Basic/1-type/1-2-function.ts
@@ -4,7 +4,7 @@
   //   return num1 + num2;
   // }
 
-  // // TypeScript
+  // // TypeScript👍👍
   // function add(num1: number, num2: number): number {
   //   return num1 + num2;
   // }
@@ -19,7 +19,7 @@
   //   });
   // }
 
-  // // TypeScript
+  // // TypeScript👍👍
   // function fetchNum(id: string): Promise<number> {
   //   // code ...
   //   // code ...
@@ -29,10 +29,12 @@
   //   });
   // }
 
-  // 최신 javascript는 물론 사용 가능
+  // 함수명 옆의 :은 해당 함수의 리턴 값을 명시해서 가독성을 높여준다.
+
+  // 최신 javascript 문법은 모두 typescript에서 사용 가능
   // 새로운 기능도 있음
   // Optional parameter
-  // lastName?: string ?을 붙일 경우 있어도 되고 없어도 된다는 뜻이다. 👍👍
+  // lastName?: string ?을 붙일 경우 해당 타입일 수도 있고, undefined일 수도 있다는 뜻이다. (44줄 참조) 👍👍
   // lastName: string| undefined 이렇게 할 수도 있지만 이럴 경우 undefined를 항상 정의해 줘야 한다.😢😢
   function printName(firstName: string, lastName?: string) {
     console.log(firstName);
@@ -43,12 +45,15 @@
   printName('Steve', undefined);
 
   // Default parameter
+  // 인자가 아무것도 없을 때 내가 원하는 기본값을 출력하고 싶다면 밑에처럼 작성하면 된다.(49줄 default message)
   function printMessage(message: string = 'default message') {
     console.log(message); 
   }
   printMessage();
 
   // Rest parameter
+  // 모든 전달되는 인자들을 원하는 타입 형태로 받아 온다.
+  // 밑에 줄의 ...numbers: number[]는 모든 전달되는 인자들을 숫자 타입의 배열 형태로 받아오는 것을 뜻한다.
   function addNumbers(...numbers: number[]): number {
     return numbers.reduce((a, b) => a + b);
   }
