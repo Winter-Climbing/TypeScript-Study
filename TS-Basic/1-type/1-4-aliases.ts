9da123a653e25933a6af4d8127d712afea23e61a
Gose <niabc70@gmail.com>
2022-09-17 20:33:58 +0900

    1-3, 1-4 array, aliases

---
+++ TS-Basic/1-type/1-4-aliases.ts
@@ -0,0 +1,27 @@
+{
+  /*
+  * Type Aliases
+  타입 커스터마이징
+  */
+
+  type Text = string
+  const name: Text = 'ellie'
+
+  type Num = number
+  type Student = {
+    name: string,
+    age: number
+  }
+  const student: Student = {
+    name: 'ellie',
+    age: 12
+  }
+
+  // String Literal Types
+  // 타입 커스터마이징 + 값 지정
+  type Name = 'name'
+  let ellieName: Name
+  ellieName = 'name'
+  type JSON = 'json'
+  const json:JSON = 'json'
+}
