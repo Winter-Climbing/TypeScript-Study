9a182c2de54552ffd9e8422740000301015639f9
Gose <97828478+Winter-Climbing@users.noreply.github.com>
2022-11-12 20:22:53 +0900

    Delete TS-Basic/3-oop directory

--- TS-Basic/3-oop/dist/3-1-without_oop.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-{
-    let coffee = 0;
-    const makeCoffee = (shot) => {
-        return (coffee += shot);
-    };
-    makeCoffee(4);
-}
--- TS-Basic/3-oop/src/3-1-without_oop.ts
+++
@@ -1,9 +0,0 @@
-{
-  let coffee = 0;
-
-  const makeCoffee = (shot: number): number => {
-    return (coffee += shot);
-  };
-
-  makeCoffee(4);
-}
